6b5383a695560fbbdbc18d5426644a29837123ed
chayanbajaj <chayanbajaj@gmail.com>
2022-08-19 13:33:37 +0530

    Layout added to pages

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+export default function Layout({title, description, kewords, children}) {
+  return (
+    <div>
+        <Head>
+            <title> {title} </title>
+            <meta name='description' content={description} />
+            <meta name='keywords' content={keywords} />
+        </Head>
+        <div>{children}</div>
+    </div>
+  )
+}
+Layout.defaultProps = {
+    title: 'DJ Events | Find the hottest parties',
+    description: 'Find the latest DJ and other musical events',
+    keywords: 'music, dj, edm, events',
+}
